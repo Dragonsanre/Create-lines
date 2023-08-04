b2552bdb84cd9209e82cfad0327f6b76e794b530
Dragonsanre <62077168+Dragonsanre@users.noreply.github.com>
2023-08-04 18:23:57 -0400

    Add files via upload

---
+++ Assignment_4/Assignment_4/MainWindow.xaml.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Assignment_4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ArrayList threadList = new ArrayList();
+
+        public ArrayList lineList = new ArrayList();
+        int count = 0;
+        bool paused = false;
+        bool started = false;
+        int currentvalue = 1;
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = TimeSpan.FromMilliseconds(50);
+            timer.Start();
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+
+            Stick stick1 = new Stick();
+            int newcounter = 0;
+            int maxlines = 0;
+            int counter = 0;
+            timer.Stop();
+            if (sldrName.Value == currentvalue)
+            {
+
+            }
+            else if (sldrName.Value > currentvalue)
+            {
+                while(sldrName.Value > currentvalue)
+                {
+                    currentvalue += 1;
+                }
+                
+                sldrName.Value = currentvalue;
+            }
+            else if (sldrName.Value < currentvalue)
+            {
+                while (sldrName.Value < currentvalue)
+                {
+                    currentvalue -= 1;
+                }
+                sldrName.Value = currentvalue;
+            }
+            if (started == true && paused == false)
+            {
+                while (newcounter < lineList.Count)
+                {
+                    maxlines = currentvalue;
+                    stick1.Assign = (Stick)lineList[newcounter];
+                    while (stick1.Lines > maxlines)
+                    {
+                        threadList.RemoveAt(0);
+                        this.stickArea.Children.Remove(stick1.RemoveLine());
+                        lineList.RemoveAt(newcounter);
+                        lineList.Insert(newcounter, stick1);
+                    }
+                    newcounter++;
+                }
+            }
+            else if(paused == true)
+            {
+                while (counter < lineList.Count)
+                {
+                    stick1.Assign = (Stick)lineList[counter];
+                    if (stick1.Lines > 0)
+                    {
+                        threadList.RemoveAt(0);
+                        this.stickArea.Children.Remove(stick1.RemoveLine());
+                        lineList[counter] = stick1;
+
+                    }
+                    counter++;
+                }
+            }
+            count = 0;
+            if (paused == false)
+            {
+                
+            }
+            
+            timer.Start();
+        }
+        
+        private void bStart_Click(object sender, RoutedEventArgs e)
+        {
+            started = true;
+            Thread t1 = new Thread(new ThreadStart(DrawLine));
+            t1.SetApartmentState(ApartmentState.STA);
+            t1.Start();
+            threadList.Add(t1);
+
+
+        }
+        private void bStop_Click(object sender, RoutedEventArgs e)
+        {
+            int newcounter = 0;
+            Stick stick1 = new Stick();
+            while(newcounter < lineList.Count)
+            {
+                stick1.Assign = (Stick)lineList[newcounter];
+                stick1.State = false;
+                lineList[newcounter] = stick1;
+                newcounter++;
+            }
+        }
+        private void bPause_Click(object sender, RoutedEventArgs e)
+        {
+            paused = true;
+        }
+        private void bResume_Click(object sender, RoutedEventArgs e)
+        {
+            paused = false;
+        }
+        public void DrawLine()
+        {
+            Stick newStick = new Stick(this.stickArea.ActualWidth, this.stickArea.ActualHeight);
+            Random random = new Random();
+            this.Dispatcher.Invoke(() =>
+            {
+                this.stickArea.Children.Add(newStick.CreateLine(random.Next(0,(int)this.stickArea.Width), random.Next(0, (int)this.stickArea.Width), random.Next(0, (int)this.stickArea.Height), random.Next(0, (int)this.stickArea.Height)));
+                lineList.Add(newStick);
+            });
+
+        }
+        public void NewLine(object stick)
+        {
+            Stick stick1 = new Stick();
+            Thread t1 = new Thread(new ParameterizedThreadStart(NewLine));
+            this.Dispatcher.Invoke(() =>
+            {
+                stick1.Assign = (Stick)stick;
+                this.stickArea.Children.Add(stick1.MoveLine());
+                Thread.Sleep(50);
+                t1.SetApartmentState(ApartmentState.STA);
+                t1.Start(stick1);
+
+                if (stick1.Lines > this.sldrName.Value)
+                {
+                    threadList.RemoveAt(0);
+                    this.stickArea.Children.Remove(stick1.RemoveLine());
+                }
+            
+            });
+
+        }
+    }
+}
---
+++ Assignment_4/Assignment_4/Stick.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Collections;
+
+namespace Assignment_4
+{
+    class Stick
+    {
+        private double x1;
+        private double x2;
+        private double y1;
+        private double y2;
+        private double linecount;
+        public double winWidth = 0;
+        public double winHeight = 0;
+        public int x1Dir = 0;
+        public int x2Dir = 0;
+        public int y1Dir = 0;
+        public int y2Dir = 0;
+        private bool state = true;
+        
+        ArrayList Sticklist = new ArrayList();
+        public bool State
+        {
+            get { return this.state; }
+            set { this.state = value; }
+        }
+        public Stick Assign
+        {
+            get { return this; }
+            set
+            {
+                this.x1 = value.X1;
+                this.x2 = value.X2;
+                this.y1 = value.Y1;
+                this.y2 = value.Y2;
+                this.winWidth = value.winWidth;
+                this.winHeight = value.winHeight;
+                this.x1Dir = value.x1Dir;
+                this.x2Dir = value.x2Dir;
+                this.y1Dir = value.y1Dir;
+                this.y2Dir = value.y2Dir;
+                this.linecount = value.Sticklist.Count;
+                this.Sticklist = value.Sticklist;
+                this.State = value.State;
+            }
+        }
+        public double Lines
+        {
+            get { return this.linecount; }
+            set { this.linecount = value; }
+        }
+        public double X1
+        {
+            get { return this.x1; }
+            set { this.x1 = value; }
+        }
+        public double X2
+        {
+            get { return this.x2; }
+            set { this.x2 = value; }
+        }
+        public double Y1
+        {
+            get { return this.y1; }
+            set { this.y1 = value; }
+        }
+        public double Y2
+        {
+            get { return this.y2; }
+            set { this.y2 = value; }
+        }
+        public Stick(double windowWidthIn, double windowHeightIn)
+        {
+            this.winWidth = windowWidthIn;
+            this.winHeight = windowHeightIn;
+            x1Dir = 1;
+            x2Dir = 0;
+            y1Dir = 0;
+            y2Dir = 1;
+        }
+        public Stick()
+        {
+
+        }
+        public Line CreateLine(double x1, double x2, double y1, double y2)
+        {
+            Line newLine = new Line();
+            int num = RandomInt();
+            if (num == 1)
+            {
+                newLine.Stroke = Brushes.Red;
+            }
+            if (num == 2)
+            {
+                newLine.Stroke = Brushes.Blue;
+            }
+            if (num == 3)
+            {
+                newLine.Stroke = Brushes.Purple;
+            }
+            this.X1 = x1;
+            this.X2 = x2;
+            this.Y1 = y1;
+            this.Y2 = y2;
+            newLine.X1 = x1;
+            newLine.X2 = x2;
+            newLine.Y1 = y1;
+            newLine.Y2 = y2;
+            Lines++;
+            newLine.StrokeThickness = 4;
+            Sticklist.Add(newLine);
+            return newLine;
+        }
+        public int RandomInt()
+        {
+            Random randNum = new Random();
+            return randNum.Next(1, 4);
+        }
+        public Line MoveLine()
+        {
+            Line newLine = new Line();
+            int num = RandomInt();
+            if (X1 >= winWidth || x1 <= 0)
+            {
+                x1Dir = (x1Dir + 1) % 2;
+            }
+            if (X2 >= winWidth || x2 <= 0)
+            {
+                x2Dir = (x2Dir + 1) % 2;
+            }
+            if (Y1 >= winHeight || y1 <= 0)
+            {
+                y1Dir = (y1Dir + 1) % 2;
+            }
+            if (Y2 >= winHeight || y2 <= 0)
+            {
+                y2Dir = (y2Dir + 1) % 2;
+            }
+            if (x1Dir == 0)
+            {
+                X1 += 10;
+            }
+            else
+            {
+                X1 -= 10;
+            }
+            if (x2Dir == 0)
+            {
+                X2 += 10;
+            }
+            else
+            {
+                X2 -= 10;
+            }
+            if (y1Dir == 0)
+            {
+                Y1 += 10;
+            }
+            else
+            {
+                Y1 -= 10;
+            }
+            if (y2Dir == 0)
+            {
+                Y2 += 10;
+            }
+            else
+            {
+                Y2 -= 10;
+            }
+            newLine.X1 = x1;
+            newLine.X2 = x2;
+            newLine.Y1 = y1;
+            newLine.Y2 = y2;
+            if (num == 1)
+            {
+                newLine.Stroke = Brushes.Red;
+            }
+            if (num == 2)
+            {
+                newLine.Stroke = Brushes.Blue;
+            }
+            if (num == 3)
+            {
+                newLine.Stroke = Brushes.Purple;
+            }
+            newLine.StrokeThickness = 4;
+            Lines++;
+            Sticklist.Add(newLine);
+            return newLine;
+        }
+        public Line RemoveLine()
+        {
+            Line line = (Line)Sticklist[0];
+            Sticklist.RemoveAt(0);
+            Lines -= 1;
+            return line;
+        }
+
+    }
+
+}
